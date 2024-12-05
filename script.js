547939abf79c2d1f4cd96759b67841dd9e84fbb3
Charlie Leedham <charlieleedham1@gmail.com>
2024-12-05 13:07:06 +0000

    finalised stuff

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let currentOrientation = localStorage.getItem('orientation') || "portrait"
+
 function addRow(event, tableIndex) {
     const tableBody = event.target.closest('table').querySelector('tbody')
     const newRow = document.createElement('tr')
@@ -22,10 +24,10 @@ function addRow(event, tableIndex) {
     
     else if(tableIndex == 2) {
         newRow.innerHTML = `
-            <th style='width: 10%' contenteditable>Worker name</th>
-            <td style='width: 70%' contenteditable>CHRISTOPHER BLOODWORTH</td>
-            <td style='width: 10%; text-align: center;' contenteditable>Yes</td>
-            <td style='width: 10%; text-align: center;' contenteditable>S</td>
+            <th style='width: 17%; vertical-align: middle' contenteditable>Worker name</th>
+            <td style='width: 63%; vertical-align: middle' contenteditable>CHRISTOPHER BLOODWORTH</td>
+            <td style='width: 10%; text-align: center; vertical-align: middle' contenteditable>Yes</td>
+            <td style='width: 10%; text-align: center; vertical-align: middle' contenteditable>S</td>
             <td style='display: contents'>
                 <span onclick='removeRow()' class='remove-row'>Remove</span>
             </td>`
@@ -33,14 +35,14 @@ function addRow(event, tableIndex) {
     
     else if(tableIndex == 3) {
         newRow.innerHTML = `
-            <th contenteditable>Steps</th>
-            <td style='text-align: center;' contenteditable>NO</td>
-            <th style='max-width: 25px;' contenteditable>Hand- Power tools</th>
-            <td style='text-align: center;' contenteditable>NO</td>
-            <th style='max-width: 25px;' contenteditable>Generator</th>
-            <td style='text-align: center;' contenteditable>NO</td>
-            <th style='max-width: 25px;' contenteditable>Hop ups</th>
-            <td style='text-align: center;' contenteditable>NO</td>
+            <th style='width: 15%; max-width: 25px;' contenteditable>Steps</th>
+            <td style='text-align: center; vertical-align: middle' contenteditable>NO</td>
+            <th style='width: 15%; max-width: 25px;' contenteditable>Hand- Power tools</th>
+            <td style='text-align: center; vertical-align: middle' contenteditable>NO</td>
+            <th style='width: 15%; max-width: 25px;' contenteditable>Generator</th>
+            <td style='text-align: center; vertical-align: middle' contenteditable>NO</td>
+            <th style='width: 15%; max-width: 25px;' contenteditable>Hop ups</th>
+            <td style='text-align: center; vertical-align: middle' contenteditable>NO</td>
             <td style='display: contents'>
                 <span onclick='removeRow()' class='remove-row'>Remove</span>
             </td>`
@@ -48,11 +50,11 @@ function addRow(event, tableIndex) {
 
     else if(tableIndex == 4) {
         newRow.innerHTML = `
-            <td contenteditable>C BLOODWORTH</td>
-            <td contenteditable></td>
-            <td contenteditable>DD/MM/YYYY</td>
-            <td contenteditable>PREMIER DECORATORS UK LTD</td>
-            <td contenteditable>G COPE</td>
+            <td style='vertical-align: middle; height: 40px; text-align: center' contenteditable>C BLOODWORTH</td>
+            <td style='vertical-align: middle; height: 40px; text-align: center' contenteditable></td>
+            <td style='vertical-align: middle; height: 40px; text-align: center' contenteditable>DD/MM/YYYY</td>
+            <td style='vertical-align: middle; height: 40px; text-align: center' contenteditable>PREMIER DECORATORS UK LTD</td>
+            <td style='vertical-align: middle; height: 40px; text-align: center' contenteditable>G COPE</td>
             <td style='display: contents'>
                 <span onclick='removeRow()' class='remove-row'>Remove</span>
             </td>`
@@ -60,6 +62,7 @@ function addRow(event, tableIndex) {
 
     tableBody.appendChild(newRow)
     addPageBreaks()
+    saveContent()
 }
 
 function removeRow() {
@@ -68,6 +71,7 @@ function removeRow() {
 
     tableBody.removeChild(row)
     addPageBreaks()
+    saveContent();
 }
 
 
@@ -78,73 +82,104 @@ function mmToPx(mm) {
 
 function addPageBreaks() {
     const containers = [container1, container2, container3]
-    const existingBreaks = document.querySelectorAll('.page-break')
-    const pageBreakPx = mmToPx(210)
+    const pageBreakPx = mmToPx(currentOrientation == "portrait" ? 297 : 210)
 
-    existingBreaks.forEach(breakLine => breakLine.remove())
+    const containerBreakStates = containers.map(container => {
+        const breaks = Array.from(container.querySelectorAll('.page-break'))
+        return breaks.map(breakLine => {
+            const span = breakLine.querySelector('span')
+            return { isHidden: span.style.opacity === "0.6" }
+        })
+    })
 
-    for (let i = 0; i < containers.length; i++) {
-        const container = containers[i]
-        const containerHeight = containers[i].offsetHeight
+    containers.forEach(container => {
+        container.querySelectorAll('.page-break').forEach(breakLine => breakLine.remove())
+    })
+
+    containers.forEach((container, containerIndex) => {
+        const containerHeight = container.offsetHeight
         const numberOfBreaks = Math.floor(containerHeight / pageBreakPx)
+        const breakStates = containerBreakStates[containerIndex]
 
         for (let n = 1; n <= numberOfBreaks; n++) {
             const pageBreak = document.createElement('div')
-            const pageBreakText = document.createElement('span')
-
             pageBreak.classList.add('page-break')
 
-            pageBreakText.onclick = function(event) {
-                if(event.target.innerText == "Hide break") {
-                    event.target.innerText = "Show break"
-                    this.style.opacity = '0.6'
+            const pageBreakText = document.createElement('span')
+            pageBreakText.innerText = "Hide break"
+
+            if (breakStates && breakStates[n - 1] && breakStates[n - 1].isHidden) {
+                pageBreakText.style.opacity = '0.6'
+                pageBreak.style.background = 'none'
+                pageBreak.style.boxShadow = 'none'
+                pageBreakText.innerText = "Show break"
+            }
+
+            pageBreakText.onclick = function (event) {
+                if (event.target.innerText === "Hide break") {
+                    event.target.innerText = 'Show break'
+                    this.style.opacity = 0.6
                     this.parentElement.style.background = 'none'
                     this.parentElement.style.boxShadow = 'none'
-                }
-                else {
+                } else {
                     event.target.innerText = "Hide break"
-                    this.style.opacity = '1'
+                    this.style.opacity = 1
                     this.parentElement.style.background = 'yellow'
                     this.parentElement.style.boxShadow = '0px 0px 5px 1px black'
                 }
-            }
+            };
 
-            if(i == 0){
-                pageBreak.style.top = `${(n * pageBreakPx) + ((n * 67) + (n * 4) - 5 + .5)}px`
-
-                if (parseFloat(pageBreak.style.top) < containerHeight + 25) {
-                    container.appendChild(pageBreak)
-                }
-            }
-
-            else {
-                pageBreak.style.top = `${(n * pageBreakPx) + ((n * 67) + (n * 4) - 1 + .5)}px`
-
-                if (parseFloat(pageBreak.style.top) < containerHeight + 21) {
-                    container.appendChild(pageBreak)
-                }
-            }
-
-            pageBreakText.innerText = "Hide break"
             pageBreak.appendChild(pageBreakText)
+
+            const pageBreakTop = `${(n * pageBreakPx) + ((n * (currentOrientation == "portrait" ? 147 : 67)) + (n * 4) - (containerIndex === 0 ? 5 : 1) + 0.5)}px`
+            pageBreak.style.top = pageBreakTop
+
+            if (parseFloat(pageBreak.style.top) < containerHeight + 25) {
+                container.appendChild(pageBreak)
+            }
         }
-    }
+    })
 }
+
+function updatePageLayout(orientation) {
+    const containers = [container1, container2, container3]
+
+    if (orientation === "portrait") {
+        containers.forEach(container => {
+            container.style.maxWidth = '210mm'
+            container.style.minHeight = '297mm'
+        })
+    } else if (orientation === "landscape") {
+        containers.forEach(container => {
+            container.style.maxWidth = '297mm'
+            container.style.minHeight = '210mm'
+        })
+    }
+
+    currentOrientation = orientation
+
+    localStorage.setItem('orientation', currentOrientation)
+
+    setTimeout(() => {
+        addPageBreaks()
+    }, 1000)
+}
+
 
 async function exportToPDF(event, download = true) {
     const {jsPDF} = window.jspdf
-    const pdf = new jsPDF('landscape', 'mm', 'a4')
+    const pdf = new jsPDF(currentOrientation, 'mm', 'a4')
     const containers = [container1, container2, container3]
     let toDelete = []
     let totalHeight = -5
-    
+
     document.querySelectorAll('.add-row, .remove-row, .page-break').forEach(element => {
         element.style.display = 'none'
-    });
+    })
 
     pdf.setDocumentProperties({
         title: "Risk Assessment & Method Statement"
-    });
+    })
 
     for (let i = 0; i < containers.length; i++) {
         const container = containers[i]
@@ -152,8 +187,8 @@ async function exportToPDF(event, download = true) {
         await pdf.html(container, {
             x: -4,
             y: totalHeight,
-            width: 297,
-            windowWidth: 1223,
+            width: currentOrientation == "portrait" ? 210 : 297,
+            windowWidth: currentOrientation == "portrait" ? 900 : 1223,
             callback: function (doc) {
                 const numPages = doc.internal.pages.length
                 const pageHeight = doc.internal.pageSize.height
@@ -176,13 +211,13 @@ async function exportToPDF(event, download = true) {
 
         pdf.deletePage(pageIndexToDelete)
     }
-    
+
     const finalDocPages = pdf.internal.pages.length - 1
 
     for (let i = 1; i <= finalDocPages; i++) {
         const pageWidth = pdf.internal.pageSize.width
         const pageHeight = pdf.internal.pageSize.height
-        
+
         pdf.setPage(i)
         pdf.setFontSize(8)
         pdf.text(pageWidth - 7.5, pageHeight - 6.5, `Page  ${i}  of  ${finalDocPages}`, null, 90)
@@ -215,7 +250,143 @@ async function exportToPDF(event, download = true) {
     })
 }
 
+
+function saveContent() {
+    const containers = [container1, container2, container3]
+    const contentData = {}
+
+    containers.forEach((container, containerIndex) => {
+        const tables = container.querySelectorAll('table')
+        contentData[`container_${containerIndex}`] = []
+
+        tables.forEach((table, tableIndex) => {
+            const tableData = {
+                thead: [],
+                tbody: []
+            }
+
+            const thead = table.querySelector('thead');
+            if (thead) {
+                thead.querySelectorAll('tr').forEach((row) => {
+                    const rowData = []
+                    row.querySelectorAll('th, td').forEach((cell) => {
+                        const cellData = {
+                            tag: cell.tagName.toLowerCase(),
+                            content: cell.innerHTML,
+                            attributes: {}
+                        };
+                        Array.from(cell.attributes).forEach((attr) => {
+                            cellData.attributes[attr.name] = attr.value
+                        })
+                        rowData.push(cellData)
+                    })
+                    tableData.thead.push(rowData)
+                })
+            }
+
+            const tbody = table.querySelector('tbody');
+            if (tbody) {
+                tbody.querySelectorAll('tr').forEach((row) => {
+                    const rowData = []
+                    row.querySelectorAll('td, th').forEach((cell) => {
+                        const cellData = {
+                            tag: cell.tagName.toLowerCase(),
+                            content: cell.innerHTML,
+                            attributes: {}
+                        }
+                        Array.from(cell.attributes).forEach((attr) => {
+                            cellData.attributes[attr.name] = attr.value
+                        })
+                        rowData.push(cellData)
+                    })
+                    tableData.tbody.push(rowData)
+                })
+            }
+
+            contentData[`container_${containerIndex}`].push({
+                tableIndex,
+                tableData
+            })
+        })
+    })
+
+    localStorage.setItem('editableContent', JSON.stringify(contentData))
+}
+
+function loadContent() {
+    const savedContent = localStorage.getItem('editableContent')
+    if (savedContent) {
+        const contentData = JSON.parse(savedContent)
+
+        const containers = [container1, container2, container3]
+
+        containers.forEach((container, containerIndex) => {
+            const tables = container.querySelectorAll('table')
+            const containerData = contentData[`container_${containerIndex}`]
+
+            if (!containerData) return
+
+            containerData.forEach((tableContent, tableIndex) => {
+                const table = tables[tableIndex]
+                if (!table) return
+
+                const thead = table.querySelector('thead') || document.createElement('thead')
+                thead.innerHTML = ''
+                tableContent.tableData.thead.forEach((rowData) => {
+                    const newRow = document.createElement('tr')
+                    rowData.forEach((cellData) => {
+                        const newCell = document.createElement(cellData.tag)
+                        newCell.innerHTML = cellData.content
+
+                        Object.keys(cellData.attributes).forEach((attr) => {
+                            newCell.setAttribute(attr, cellData.attributes[attr])
+                        })
+
+                        newRow.appendChild(newCell)
+                    })
+                    thead.appendChild(newRow)
+                })
+
+                if (!table.querySelector('thead')) {
+                    table.prepend(thead)
+                }
+
+                const tbody = table.querySelector('tbody') || document.createElement('tbody')
+                tbody.innerHTML = ''
+                tableContent.tableData.tbody.forEach((rowData) => {
+                    const newRow = document.createElement('tr')
+                    rowData.forEach((cellData) => {
+                        const newCell = document.createElement(cellData.tag)
+                        newCell.innerHTML = cellData.content
+
+                        Object.keys(cellData.attributes).forEach((attr) => {
+                            newCell.setAttribute(attr, cellData.attributes[attr])
+                        })
+
+                        newRow.appendChild(newCell)
+                    })
+                    tbody.appendChild(newRow)
+                })
+
+                if (!table.querySelector('tbody')) {
+                    table.appendChild(tbody)
+                }
+            })
+        })
+    }
+}
+
+function resetDefault() {
+    const userConfirmed = confirm("Are you sure you want to reset all changes and delete the saved content?");
+    
+    if (userConfirmed) {
+        localStorage.removeItem('editableContent')
+        location.reload()
+    }
+}
+
 window.onload = function () {
+    document.querySelector('main').style.opacity = 1
 
     const container1 = document.querySelector('#container1')
     window.container1 = container1
@@ -224,10 +395,13 @@ window.onload = function () {
     const container3 = document.querySelector('#container3')
     window.container3 = container3
 
+    updatePageLayout(currentOrientation)
+    loadContent()
 }
 
 document.addEventListener('input', (event) => {
     if (event.target.hasAttribute('contenteditable')) {
-        addPageBreaks();
+        addPageBreaks()
+        saveContent()
     }
-});
+})
